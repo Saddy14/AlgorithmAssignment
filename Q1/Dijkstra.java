b64b85eb0a976ca2f0eaacbcfa474001869d8018
pohernqii <123614940+pohernqii@users.noreply.github.com>
2024-05-29 05:31:35 +0800

    Updated Dijkstra.java

--- Q1/Dijkstra.java
+++ Q1/Dijkstra.java
@@ -4,32 +4,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-import Q1.Star;
-/* 
-class Star {
-    int name;
-    int x, y, z;
 
-    public Star(int name, int x, int y, int z) {
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public double calculateDistance(Star other) {
-        return Math.sqrt(Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2) + Math.pow(other.z - this.z, 2));
-    }
-}
-*/
 
 class Edge {
     int vertex;
-    double weight;
+    double distance;
 
-    public Edge(int vertex, double weight) {
+    public Edge(int vertex, double distance) {
         this.vertex = vertex;
-        this.weight = weight;
+        this.distance = distance;
+    }
+
+   public int getvertex() { 
+       return vertex;
     }
 }
 
@@ -61,18 +48,43 @@ class Graph {
 
     public void addEdge(int sourceId, int destinationId) {
         if (stars.containsKey(sourceId) && stars.containsKey(destinationId)) {
-            double weight = stars.get(sourceId).calculateDistance(stars.get(destinationId));
-            adjacencyList.get(sourceId).add(new Edge(destinationId, weight));
-            adjacencyList.get(destinationId).add(new Edge(sourceId, weight)); 
+            double distance = stars.get(sourceId).calculateDistance(stars.get(destinationId));
+            boolean edgeExistsSource = false;
+            for (Edge edge : adjacencyList.get(sourceId)) {
+                if (edge.getvertex() == destinationId && Double.compare(edge.distance, distance) == 0) {
+                    edgeExistsSource = true;
+                    break;
+                }
+            }
+
+            // If the edge doesn't exist, add it
+            if (!edgeExistsSource) {
+                adjacencyList.get(sourceId).add(new Edge(destinationId, distance)); 
+            }   
+          //  adjacencyList.get(sourceId).add(new Edge(destinationId, weight));
+           // Check if the edge from sourceId to destinationId already exists in the adjacency list of destinationId
+        boolean edgeExists = false;
+        for (Edge edge : adjacencyList.get(destinationId)) {
+            if (edge.getvertex() == sourceId && Double.compare(edge.distance, distance) == 0) {
+                edgeExists = true;
+                break;
+            }
+        }
+        
+        // If the edge doesn't exist, add it
+        if (!edgeExists) {
+            adjacencyList.get(destinationId).add(new Edge(sourceId, distance)); 
+        }   
+            
         }
     }
 
     public void printAdjacencyList() {
         System.out.println("Adjacency List:");
-        for (int i = 1; i < adjacencyList.size()-1; i++) {
+        for (int i = 1; i < adjacencyList.size(); i++) {
             System.out.print("Star " + i + ": ");
             for (Edge edge : adjacencyList.get(i)) {
-                System.out.print("(" + edge.vertex + ", " + String.format("%.2f", edge.weight) + ") ");
+                System.out.print("(" + edge.vertex + ", " + String.format("%.2f", edge.distance) + ") ");
             }
             System.out.println();
         }
@@ -94,7 +106,7 @@ public class Dijkstra{
         Arrays.fill(distances, Double.MAX_VALUE);
         distances[sourceVertex] = 0;
 
-        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingDouble(e -> e.weight));
+        PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingDouble(e -> e.distance));
         pq.offer(new Edge(sourceVertex, 0));
 
         while (!pq.isEmpty()) {
@@ -105,7 +117,7 @@ public class Dijkstra{
 
             for (Edge edge : graph.adjacencyList.get(currentVertex)) {
                 if (!visited[edge.vertex]) {
-                    double newDist = distances[currentVertex] + edge.weight;
+                    double newDist = distances[currentVertex] + edge.distance;
                     if (newDist < distances[edge.vertex]) {
                         distances[edge.vertex] = newDist;
                         predecessors[edge.vertex] = currentVertex;
@@ -120,7 +132,7 @@ public class Dijkstra{
     }
 
     private void printDistances(double[] distances, int sourceVertex) {
-        System.out.println("Shortest paths from vertex " + sourceVertex + ":");
+        System.out.println("\nShortest paths from vertex " + sourceVertex + ":");
         for (int i = 1; i < distances.length; i++) {
             System.out.println("To vertex " + i + " - " + (distances[i] == Double.MAX_VALUE ? "No path" : String.format("%.2f", distances[i])));
         }
@@ -145,7 +157,19 @@ public class Dijkstra{
         System.out.print(vertex);
     }
 
-    private static void initializeEdges(Graph graph) {
+    public static void initializeEdges(Graph graph) {
+        /* 
+        Star star=new Star();
+      
+        System.out.println(star.getConnectedStars());
+        for (int i=1; i<=20; i++){
+            for (Star s :star.getConnectedStars()) {
+                graph.addEdge(i, s.getName());
+             }
+        }
+       */
+     
+       /* 
         graph.addEdge(1, 2);
         graph.addEdge(1, 3);
         graph.addEdge(1, 4);
@@ -166,7 +190,21 @@ public class Dijkstra{
         graph.addEdge(19, 1);
         graph.addEdge(19, 20);
         graph.addEdge(20, 2);
-        
+        */
+        String filePath = "connected_stars.csv";
+        try (BufferedReader reader= new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine(); 
+            while ((line = reader.readLine()) != null) {
+                // Use comma as separator
+                String[] edge = line.split(",");
+                int vertex1 = Integer.parseInt(edge[0]);
+                int vertex2 = Integer.parseInt(edge[1]);
+
+                graph.addEdge(vertex1, vertex2);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -192,7 +230,7 @@ public class Dijkstra{
         initializeEdges(graph);
       
         //uncomment this for adjacency list
-       // graph.printAdjacencyList();
+        graph.printAdjacencyList();
 
         Dijkstra dijkstra = new Dijkstra(graph);
 
