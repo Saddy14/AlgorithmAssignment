514208290a62042e4e4851556851faafe24d518b
pohernqii <123614940+pohernqii@users.noreply.github.com>
2024-05-30 23:54:46 +0800

    Update KruskalAlgorithm.java

--- Q1/KruskalAlgorithm.java
+++ Q1/KruskalAlgorithm.java
@@ -7,9 +7,9 @@ import java.util.*;
 
 class Edge{
     int src, dest;
-    double distance;
+    int distance;
 
-    public Edge(int src, int dest, double distance) {
+    public Edge(int src, int dest, int distance) {
         this.src = src;
         this.dest = dest;
         this.distance = distance;
@@ -17,16 +17,8 @@ class Edge{
 
 }
 
-class Union {
+public class KruskalAlgorithm {
     private int[] parent;
-    public Union(int n) {
-       // this.n = n;
-        //An array that holds the parent of each element
-        parent = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
 
     public int find(int i) {
         //This condition checks whether the current element i is its own parent. 
@@ -36,48 +28,46 @@ class Union {
         return parent[i];
     }
 
-    public void union(int x, int y) {   //1,3
-        parent[y] = x;
-      //  System.out.println(Arrays.toString(parent));
-    }
-}
-
-public class KruskalAlgorithm {
     public void Kruskal(ArrayList<Edge> edges, int V) {
         //sorts the edges of the graph based on their weights in ascending order.
         // Sort using Comparator
         Collections.sort(edges, new Comparator<Edge>() {
             @Override
             public int compare(Edge edge1, Edge edge2) {
-                return Double.compare(edge1.distance, edge2.distance);
+                return Integer.compare(edge1.distance, edge2.distance);
             }
         });
 
-        Union u = new Union(V);
+        parent = new int[V+1];
+        for (int i = 0; i <= V; i++) {
+            parent[i] = i;
+        }
+
         int mst_weight = 0;
         List<Edge> mst = new ArrayList<>();
 
         for (Edge edge : edges) {
             //
-            int x = u.find(edge.src);
-            int y = u.find(edge.dest);
+            int x = find(edge.src);
+            int y = find(edge.dest);
 
            // System.out.println(x);
             //System.out.println(y);
             //If x and y are different, it indicates that edge.src and edge.dest are in different subsets, and the edge can be safely added without forming a cycle, 
             if (x != y) {
                 mst.add(edge);
-                u.union(x, y);
+                //u.union(x, y);
+                parent[y] = x;
                 mst_weight += edge.distance;
             }
             if (mst.size() == V - 1) break;
         }
 
-        System.out.println("Edges in the constructed MST");
+        System.out.println("Edges in MST");
         for (Edge edge : mst) {
-            System.out.println(edge.src + " -- " + edge.dest + "  distances  " + edge.distance);
+            System.out.println(edge.src + " -- " + edge.dest + "  distance:  " + edge.distance);
         }
-        System.out.println("total: " + mst_weight);
+        System.out.println("\ntotal MST: " + mst_weight);
     }
 
     public static void main(String[] args) {
@@ -112,7 +102,7 @@ public class KruskalAlgorithm {
                 int star2 = Integer.parseInt(values[1]);
                 double[] pos1 = starPositions.get(star1);
                 double[] pos2 = starPositions.get(star2);
-                double distance = Star.calculateDistance(pos1, pos2);
+                int distance = Star.calculateDistance(pos1, pos2);
                 edges.add(new Edge(star1, star2, distance));
             }
         } catch (IOException e) {
